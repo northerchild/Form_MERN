cdd6f62fda8d996da5634141c2dabb1c941ff9c1
northerchild <osarus-13@hotmail.com>
2021-02-12 23:06:20 -0500

    se crean las variables de entorno y se conecta con mongo

--- servidor/index.js
+++ servidor/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const cors = require('cors');
+const conectarDB = require('./config/db');
+const cors = require('cors')
 //Crear el servidor
 const app = express();
+//conectar a la base de datos
+conectarDB();
 //habilitar cors
 app.use(cors());
 //Habilitar express.json
 app.use(express.json({extended:true}))
 //Puerto
 const PORT = process.env.PORT || 4000;
+//arrancar el server
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`)
 })
